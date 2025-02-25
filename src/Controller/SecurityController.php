9d647ce932672077e78746275cfb54f9bcc818e9
Victor G Andrade <victor@serverti.com.br>
2025-02-25 18:11:07 -0300

    Adicionando dependencias para os proximos modulos

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,14 +4,11 @@ namespace App\Controller;
 
 use LogicException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
 
-class SecurityController extends AbstractController implements AuthenticationEntryPointInterface
+class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
@@ -36,8 +33,5 @@ class SecurityController extends AbstractController implements AuthenticationEnt
         throw new LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 
-    public function start(Request $request, ?AuthenticationException $authException = null): Response
-    {
-        // TODO: Implement start() method.
-    }
+
 }
